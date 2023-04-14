ce0cf8583eb7f6f64ccec21be90f45197b549c03
GoodMachineOil <86229912+GoodMachineOil@users.noreply.github.com>
2023-04-15 00:55:18 +0800

    "the latest"

--- StackAndQueue/src/main/java/org/example/前k个高频元素leetcode347/Solution.java
+++ StackAndQueue/src/main/java/org/example/前k个高频元素leetcode347/Solution.java
@@ -1,4 +1,9 @@
 package org.example.前k个高频元素leetcode347;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 /**
  * @Author: zc
  * @Description:
@@ -14,8 +19,19 @@ package org.example.前k个高频元素leetcode347;
 //要统计元素出现频率,一类的问题可以使用map来进行统计
 //对频率排序 -----这里我们可以使用一种 容器适配器就是优先级队列
 //找出前K个高频元素,这里我们可以使用一种 容器适配器就是优先级队列。
+    //基于小顶堆实现
 public class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-
+        //Map.getOrDefault(Object key, V defaultValue);
+        //如果在Map中存在key，则返回key所对应的的value。
+        //如果在Map中不存在key，则返回默认值。
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num, 0)+1);
+        }
+        //在优先队列中存储二元组(num,cnt),cnt表示元素值在数组中的出现次数
+        //出现次数按从队头到队尾的顺序 从大到小排，出现次数最低的在队头（相当于小顶堆)
+        PriorityQueue<int[]> pd = new PriorityQueue<>((pair1,pair2)->(pair1[1]-pair2[1]));
+        for(Map.Entry<>)
     }
 }
