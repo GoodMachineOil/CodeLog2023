5b50e246c05b73e47a679ed443ef2f22f63cef71
GoodMachineOil <86229912+GoodMachineOil@users.noreply.github.com>
2023-04-21 04:10:31 +0800

    用递归法求解路径之和的问题

---
+++ Tree/src/main/java/org/example/路径总和leetcode112/TreeNode.java
@@ -0,0 +1,20 @@
+package org.example.路径总和leetcode112;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
