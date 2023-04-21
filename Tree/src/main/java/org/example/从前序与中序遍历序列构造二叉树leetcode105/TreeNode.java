79c586842fb77b2f9a2e3d391e86b454344f84ce
GoodMachineOil <86229912+GoodMachineOil@users.noreply.github.com>
2023-04-21 16:04:45 +0800

    从前序与中序遍历序列构造二叉树

---
+++ Tree/src/main/java/org/example/从前序与中序遍历序列构造二叉树leetcode105/TreeNode.java
@@ -0,0 +1,20 @@
+package org.example.从前序与中序遍历序列构造二叉树leetcode105;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
