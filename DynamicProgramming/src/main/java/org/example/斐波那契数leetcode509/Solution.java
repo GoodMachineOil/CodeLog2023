246640988dd812a1c9df004c7d3c1eb5d4f309ce
GoodMachineOil <86229912+GoodMachineOil@users.noreply.github.com>
2023-04-25 20:45:00 +0800

    动规-斐波那契数

---
+++ DynamicProgramming/src/main/java/org/example/斐波那契数leetcode509/Solution.java
@@ -0,0 +1,46 @@
+package org.example.斐波那契数leetcode509;
+
+import java.util.Arrays;
+
+/**
+ * @Description:
+ * 斐波那契数 （通常用 F(n) 表示）形成的序列称为 斐波那契数列 。
+ * 该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ * F(0) = 0，F(1) = 1
+ * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+ * 给定 n ，请计算 F(n) 。
+ *
+ * 示例 1：
+ * 输入：n = 2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+ * 示例 2：
+ * 输入：n = 3
+ * 输出：2
+ * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+ * 示例 3：
+ * 输入：n = 4
+ * 输出：3
+ * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+ */
+//确定dp数组
+// 确定递推公式
+// dp数组如何初始化
+// 确定遍历顺序
+// 举例推导dp数组
+public class Solution {
+    public int fib(int n) {
+        if(n <= 1){
+            return n;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int index = 2; index <= n; index++) {
+            dp[index] = dp[index - 1] + dp[index - 2];
+
+        }
+        return dp[n];
+    }
+}
+
