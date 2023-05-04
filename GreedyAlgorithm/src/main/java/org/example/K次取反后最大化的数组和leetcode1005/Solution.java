0847a1fea82c8abe4a6ecf975bfa213009184f3b
GoodMachineOil <86229912+GoodMachineOil@users.noreply.github.com>
2023-05-04 23:24:44 +0800

    贪心算法求解K次取反后最大的数组和

---
+++ GreedyAlgorithm/src/main/java/org/example/K次取反后最大化的数组和leetcode1005/Solution.java
@@ -0,0 +1,58 @@
+package org.example.K次取反后最大化的数组和leetcode1005;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * @Description:
+给你一个整数数组 nums 和一个整数 k ，按以下方法修改该数组：
+选择某个下标 i 并将 nums[i] 替换为 -nums[i] 。
+重复这个过程恰好 k 次。可以多次选择同一个下标 i 。
+以这种方式修改数组后，返回数组 可能的最大和 。
+
+示例 1：
+输入：nums = [4,2,3], k = 1
+输出：5
+解释：选择下标 1 ，nums 变为 [4,-2,3] 。
+示例 2：
+输入：nums = [3,-1,0,2], k = 3
+输出：6
+解释：选择下标 (1, 2, 2) ，nums 变为 [3,1,0,2] 。
+示例 3：
+输入：nums = [2,-3,-1,5,-4], k = 2
+输出：13
+解释：选择下标 (1, 4) ，nums 变为 [2,3,-1,5,4] 。
+ */
+//第一步：将数组按照绝对值大小从大到小排序，注意要按照绝对值的大小
+//第二步：从前向后遍历，遇到负数将其变为正数，同时K--
+//第三步：如果K还大于0，那么反复转变数值最小的元素，将K用完
+//第四步：求和
+public class Solution {
+    //将数组按照绝对值大小从大到小排序，注意要按照绝对值的大小
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        //IntStream.of(nums)：将传入的int数组nums转换成一个IntStream。
+        //.boxed()：将IntStream转换为一个Stream<Integer>，这样就可以使用一些Stream API提供的更高级别的操作。
+        //.sorted((o1, o2) -> Math.abs(o2) - Math.abs(o1))：根据绝对值进行排序，使用一个Lambda表达式作为比较器，它返回第二个参数的绝对值减去第一个参数的绝对值，这样会首先将绝对值大的数字排在前面。
+        //.mapToInt(Integer::intValue)：将Stream<Integer>转换为一个IntStream。
+        //.toArray()：将IntStream转换回一个int数组
+        nums = IntStream.of(nums)
+                .boxed()
+                .sorted((o1,o2) -> Math.abs(o2) - Math.abs(o1))
+                .mapToInt(Integer::intValue).toArray();
+        int len = nums.length;
+        //从前向后遍历，遇到负数将其变为正数，同时K--
+        for(int i = 0 ; i < len ; i++){
+            if(nums[i] < 0 && k >0){
+                nums[i] = -nums[i];
+                k--;
+            }
+        }
+        // 如果K还大于0，那么反复转变数值最小的元素，将K用完
+        if(k % 2 == 1){
+            nums[len - 1] = -nums[len - 1];
+        }
+        //Arrays.stream(nums) 创建了一个 IntStream 流，将 nums 数组中的元素装箱成 Integer 类型，而后执行 sum() 方法来求和。
+        // 具体来说，Arrays.stream(nums) 方法会返回一个流，该流包含了数组 nums 中的所有元素，sum() 方法会将这些元素相加并返回它们的和。
+        return Arrays.stream(nums).sum();
+    }
+}
