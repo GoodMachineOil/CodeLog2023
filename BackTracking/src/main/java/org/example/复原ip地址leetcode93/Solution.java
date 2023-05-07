855bcb35ad5fe85e97df8ac5fe522661f44d9cbb
GoodMachineOil <86229912+GoodMachineOil@users.noreply.github.com>
2023-05-07 23:40:06 +0800

    回溯算法求解ip地址复原问题

--- BackTracking/src/main/java/org/example/复原ip地址leetcode93/Solution.java
+++ BackTracking/src/main/java/org/example/复原ip地址leetcode93/Solution.java
@@ -34,10 +34,54 @@ public class Solution {
         if(s.length() > 12){
             return resList;
         }
+        backTracking(s,0,0);
         return resList;
     }
+    // startIndex: 搜索的起始位置， pointNum:添加逗点的数量
     public void backTracking(String s, int startIndex, int pointNum){
-        if()
+        if(pointNum == 3){
+            // 逗点数量为3时，分隔结束
+            // 判断第四段⼦字符串是否合法，如果合法就放进result中
+            if(isValid(s,startIndex,s.length()-1 )){
+                resList.add(s);
+            }
+            return;
+        }
+        for(int i = startIndex; i<s.length(); i++){
+            if(isValid(s,startIndex,i)){
+                //在str的后⾯插⼊⼀个逗点
+                s = s.substring(0,i+1) + "." + s.substring(i+1);
+                pointNum++;
+                // 插⼊逗点之后下⼀个⼦串的起始位置为i+2
+                backTracking(s,i+2,pointNum);
+                pointNum--;
+                // 回溯删掉逗点
+                s=s.substring(0,i+1)+s.substring(i+2);
+            }else{
+                break;
+            }
+        }
+    }
+    // 判断字符串s在左闭⼜闭区间[start, end]所组成的数字是否合法
+    public boolean isValid(String s,int start, int end) {
+        if(start > end){
+            return false;
+        }
+        //0字开头的数字不合法
+        if(s.charAt(start) == '0' && start != end){
+            return false;
+        }
+        int num = 0;
+        for(int i = start; i <= end ; i++){
+            if(s.charAt(i) > '9' || s.charAt(i) < '0'){// 遇到⾮数字字符不合法
+                return false;
+            }
+            num = num * 10 + (s.charAt(i) - '0');
+            if(num > 255){// 如果⼤于255了不合法
+                return false;
+            }
+        }
+        return true;
     }
 
 }
