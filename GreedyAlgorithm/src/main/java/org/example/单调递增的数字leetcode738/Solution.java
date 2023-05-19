8ac072f3106f553dd214a47550dda5e127906857
GoodMachineOil <86229912+GoodMachineOil@users.noreply.github.com>
2023-05-19 23:52:35 +0800

    贪心算法求解单调递增的数字

---
+++ GreedyAlgorithm/src/main/java/org/example/单调递增的数字leetcode738/Solution.java
@@ -0,0 +1,12 @@
+package org.example.单调递增的数字leetcode738;
+
+public class Solution {
+    public int monotoneIncreasingDigits(int n) {
+        String s = String.valueOf(n);
+        char[] ch = s.toCharArray();
+        int flag = 0;
+        for(int i = s.length()-1; i >= 0 ; i++){
+
+        }
+    }
+}
