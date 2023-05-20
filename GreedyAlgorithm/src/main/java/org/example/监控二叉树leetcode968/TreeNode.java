96971febc757a2b1d1e4e61e098d019f9357a44d
GoodMachineOil <86229912+GoodMachineOil@users.noreply.github.com>
2023-05-20 20:32:26 +0800

    监控二叉树 贪心算法  求解局部最优推断出全局最优

---
+++ GreedyAlgorithm/src/main/java/org/example/监控二叉树leetcode968/TreeNode.java
@@ -0,0 +1,20 @@
+package org.example.监控二叉树leetcode968;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
