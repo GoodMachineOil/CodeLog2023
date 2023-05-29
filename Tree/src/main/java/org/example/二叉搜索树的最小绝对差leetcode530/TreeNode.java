b1b867ef287b659a73d03d815130933150553104
GoodMachineOil <86229912+GoodMachineOil@users.noreply.github.com>
2023-05-29 23:01:21 +0800

    、

---
+++ Tree/src/main/java/org/example/二叉搜索树的最小绝对差leetcode530/TreeNode.java
@@ -0,0 +1,20 @@
+package org.example.二叉搜索树的最小绝对差leetcode530;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode() {
+    }
+}
