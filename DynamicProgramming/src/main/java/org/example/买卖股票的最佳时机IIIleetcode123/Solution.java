76d395134147cee04a4b477a38778bb1dcbfaf34
GoodMachineOil <86229912+GoodMachineOil@users.noreply.github.com>
2023-06-15 22:43:13 +0800

    .

--- DynamicProgramming/src/main/java/org/example/买卖股票的最佳时机IIIleetcode123/Solution.java
+++ DynamicProgramming/src/main/java/org/example/买卖股票的最佳时机IIIleetcode123/Solution.java
@@ -1,4 +1,7 @@
 package org.example.买卖股票的最佳时机IIIleetcode123;
+
+import sun.security.util.Length;
+
 /**
  * @Author: zc
  * @Description:
@@ -26,5 +29,27 @@ package org.example.买卖股票的最佳时机IIIleetcode123;
  * 输出：0
  */
 public class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length == 0||prices== null){
+            return 0;
+        }
+        /*
+         * 定义 5 种状态:
+         * 0: 没有操作, 1: 第一次买入, 2: 第一次卖出, 3: 第二次买入, 4: 第二次卖出
+         */
+        int[][] dp = new int[prices.length][5];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        dp[0][2] = 0;
+        dp[0][3] = -prices[0];
+        dp[0][4] = 0;
 
+        for(int i = 1; i < prices.length ; i++){
+            dp[i][1] = Math.max(dp[i-1][1],-prices[i]);
+            dp[i][2] = Math.max(dp[i-1][2],dp[i-1][1] + prices[i]);
+            dp[i][3] = Math.max(dp[i-1][3],dp[i-1][2] - prices[i]);
+            dp[i][4] = Math.max(dp[i-1][4],dp[i-1][3] + prices[i]);
+        }
+        return dp[prices.length-1][4];
+    }
 }
